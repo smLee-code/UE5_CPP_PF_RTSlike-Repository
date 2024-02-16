93103f15d9f392b9aa02ba9138b8a77db9eb9429
Sang-Myeong Lee <teamjuji5@naver.com>
2024-02-16 19:00:45 +0900

    Clear

--- CPP_PF_RTSlike/Intermediate/Build/Win64/x64/CPP_PF_RTSlikeEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/user/Documents/Unreal Projects/UE5_CPP_PF_RTSlike-Repository/CPP_PF_RTSlike/Intermediate/Build/Win64/x64/CPP_PF_RTSlikeEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
--- CPP_PF_RTSlike/Source/CPP_PF_RTSlike/CPP_PF_RTSlike.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CPP_PF_RTSlike.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_PF_RTSlike, "CPP_PF_RTSlike" );
-
-DEFINE_LOG_CATEGORY(LogCPP_PF_RTSlike)
- 
